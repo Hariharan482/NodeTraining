f1036ab16970edcb4c7420d5689151da75b0d4d4
Hariharan482 <125631751+Hariharan482@users.noreply.github.com>
2023-04-06 14:54:46 +0530

    ex2

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -1,20 +1,42 @@
-let http=require('http');
-let fs=require('fs');
+let http = require('http');
+let fs = require('fs');
 
-http.createServer(async(req,res,err)=>{
-    var randomColors=[]; 
-    let data =await fs.readFileSync('../color_ palette.json',"utf-8");
-    let data_arr = JSON.parse(data);
-    let i=0;
-    while(i++<5){
-        randomColors.push(data_arr[Math.floor(Math.random()*data_arr.length)]);
-    }
-    fs.writeFileSync('./ex2_random_color.json',JSON.stringify(randomColors),"utf-8",(err)=>{
-            if(err)
-                console.log(err); 
+//creating server
+http.createServer(async (req, res, err) => {
+    let randomColors=[];  
+     //reading file
+    await fs.readFile('color_ palette.json', (err, data) => {
+        if (err) {
+            console.log("no such file");
+        }
+        else {
+            if (data.length != 0) {
+                let data_arr = JSON.parse(data);
+                let i = 0;
+                //pushing random 5 colors to an array   
+                while (i++ < 5) {
+                    // console.log(typeof(data_arr));
+                    randomColors.push(data_arr[Math.floor(Math.random() * data_arr.length)]);
+                }
+                console.log(randomColors);
+                //writing random 5 colors to a file
+                fs.writeFile('random_color.json', JSON.stringify(randomColors), (err) => {
+                    if (err)
+                        console.log(err);
+                })
+                console.log(req.headers);
+                // setting header of res obj
+                res.writeHead(200,{'Content-Type': 'application/json'});
+                // res.setHeader('Content-Type', 'application/json');
+                console.log({randomColors});
+                res.write(JSON.stringify(randomColors));
+                res.end();
+            }
+            else {
+                console.log("empty file");
+            }
+
+        }
     });
-    let getColors= await fs.readFileSync('ex2_random_color.json','utf-8');
-    console.log(getColors);
-    res.write(JSON.stringify(randomColors));
-    res.end();
-}).listen(4000);
+   
+}).listen(80);
